eafd11e8713a4592f886f119544783f3e9834f98
Mauricio Buffon <mauricio.buffon@gmail.com>
2022-11-17 18:40:11 -0300

    dropdown

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import Accordion from "./Accordion";
 import Search from "./Search";
+import Dropdown from "./Dropdown";
 
 const items = [
   {
@@ -17,10 +18,31 @@ const items = [
   },
 ];
 
+const options = [
+  {
+    label: "The Color Red",
+    value: "red",
+  },
+  {
+    label: "The Color Green",
+    value: "green",
+  },
+  {
+    label: "A Shade of Blue",
+    value: "blue",
+  },
+];
+
 const App = () => {
+  const [selected, setSelected] = useState(null);
+
   return (
     <div>
-      <Search />
+      <Dropdown
+        selected={selected}
+        onSelectedChange={setSelected}
+        options={options}
+      />
     </div>
   );
 };
---
+++ src/components/Dropdown.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect, useRef } from "react";
+
+const Dropdown = ({ options, selected, onSelectedChange }) => {
+  const outterDiv = useRef();
+  const [open, setOpen] = useState(false);
+
+  const onBodyClick = (e) => {
+    if (!outterDiv.current.contains(e.target)) {
+      setOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.body.addEventListener("click", onBodyClick);
+
+    return () => {
+      document.body.removeEventListener("click", onBodyClick);
+    };
+  }, []);
+
+  return (
+    <div className="ui form" ref={outterDiv}>
+      <div className="field">
+        <label className="label">Select a Color</label>
+        <div
+          className={`ui selection dropdown ${open ? "visible active" : ""}`}
+          onClick={(e) => setOpen(!open)}
+        >
+          <i className="dropdown icon"></i>
+          <div className="text">
+            {selected ? selected.label : "Select color"}
+          </div>
+          <div className={`menu ${open ? "visible transition" : ""}`}>
+            {options.map((option) => {
+              if (selected && option.value === selected.value) {
+                return null;
+              }
+
+              return (
+                <div
+                  className="item"
+                  key={option.value}
+                  onClick={(e) => onSelectedChange(option)}
+                >
+                  {option.label}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+      {selected ? (
+        <div className="field">
+          <p style={{ color: selected.value }}>
+            {`This text is ${selected.value}`}
+          </p>
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+export default Dropdown;
